859f172379c649b0c62f450b37cec3ed30b68dc9
leejun <93887188+lkim0402@users.noreply.github.com>
2026-02-04 16:56:54 +0900

    Update Problem12.java

--- src/leejun/week2/Problem12.java
+++ src/leejun/week2/Problem12.java
@@ -4,22 +4,20 @@ import java.util.*;
 
 public class Problem12 {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new int[]{1, 2, 3, 2, 3}));
+        System.out.println(Arrays.toString(solution(new int[]{1, 2, 3, 2, 3})));
     }
 
     public static int[] solution(int[] prices) {
         int[] res = new int[prices.length];
-        Stack<int[]> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
         int len = prices.length;
 
         for (int i = 0; i < len; i++) {
-            while (!stack.isEmpty() && stack.peek()[1] > prices[i]) {
-                int[] pop = stack.pop();
-                int index = pop[0];
-                int val = pop[1];
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
                 res[index] = i - index;
             }
-            stack.push(new int[] {i, prices[i]});
+            stack.push(i);
         }
 
         // filling 0
