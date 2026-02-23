a33917d90cc709531c9cb3a5fc492ce32c704b6f
coco <meunse1999@gmail.com>
2026-02-23 23:47:30 +0900

    문제16번

---
+++ src/coco/Queue/P16.java
@@ -0,0 +1,32 @@
+package coco.Queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class P16 {
+    public int[] solution(int [] progresses, int [] speeds) {
+        Queue<Integer> queue = new ArrayDeque<>();
+
+        int n = progresses.length;
+        int[] dayLeft = new int[n];
+        for (int i = 0; i < n; i++) {
+            dayLeft[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+        }
+
+        int count = 0;
+        int maxDay = dayLeft[0]; //가장 앞에 있는 작업이 기준이 됨.
+        for (int i =0; i <n; i++) {
+            if(maxDay >= dayLeft[i]) {
+                count++;
+            } else {
+                queue.add(count);
+                count = 1;
+                maxDay = dayLeft[i];
+            }
+        }
+        queue.add(count);
+        return queue.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+}
